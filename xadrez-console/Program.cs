e56b5c67c45c1c490c5df7c774ced168845117fd
Kaio Teles <kaiolunateles@gmail.com>
2019-08-14 00:12:10 -0300

    Adding the class Position.

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Tabuleiro;
 
 namespace xadrez_console
 {
@@ -10,6 +7,9 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
+            Posicao p = new Posicao(3, 4);
+
+            Console.WriteLine(p);
         }
     }
 }
